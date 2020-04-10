5107451ab09104c2c228a1285cd3988eb9899fdc
jpradtke <jan.peter.radtke@googlemail.com>
2020-04-10 14:14:17 +0200

    setState added

--- my-app/src/Game/App.js
+++ my-app/src/Game/App.js
@@ -6,12 +6,25 @@ class App extends React.Component{
         this.state = {
             count : 0
         }
+        this.handleClick = this.handleClick.bind(this)
     }
+
+    handleClick (){
+        return(
+            this.setState(
+                prevState => {
+                    return{
+                            count: prevState.count + 1
+                        }
+                } )
+        )
+    }
+
     render(){
         return(
             <div className = "gameDiv">
                 <h1>{this.state.count}</h1>
-                <button className = "gameButton">Count</button>
+                <button className = "gameButton" onClick = {this.handleClick} >Count</button>
             </div>
 
         )
