ba671ba1a74894138d9ba0c6e3e71cfde9cad7d6
jpradtke <jan.peter.radtke@googlemail.com>
2020-04-10 12:22:35 +0200

    add first class

--- my-app/src/Classes/App.js
+++ my-app/src/Classes/App.js
@@ -1,9 +1,17 @@
 import React from "react"
 
-function App() {
+/* function App() {
     return (
         <h1>Hello World!</h1>
     )
+} */
+
+class App extends React.Component{
+    render(){
+        return (
+            <h1>Hello World from Classes!</h1>
+        )
+    }
 }
 
 export default App
